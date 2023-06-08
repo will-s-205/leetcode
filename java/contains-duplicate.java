329e1787bfd9fed547d1edeb46879e7984b829a2
William S <will.step.205@gmail.com>
2023-06-08 09:57:23 -0700

    contains duplicate CC renamed

--- java/contains-duplicate.java
+++ java/contains-duplicate.java
@@ -1,9 +1,12 @@
+import java.util.HashSet;
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> hset = new HashSet();
 
-        for(int i=0;i<nums.length;i++){
-            if(hset.contains(nums[i])) return true;
+        for (int i = 0; i < nums.length; i++) {
+            if (hset.contains(nums[i]))
+                return true;
             hset.add(nums[i]);
         }
         return false;
