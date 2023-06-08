3e9fd9e71838c60724dd559c251d0bd997bc7543
William S <will.step.205@gmail.com>
2023-06-08 09:33:36 -0700

    Update best-time-to-buy-and-sell-stock.java

--- java/best-time-to-buy-and-sell-stock.java
+++ java/best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int left=0;
-        int right=1;
-        int maxProfit=0;
+        int left = 0;
+        int right = 1;
+        int maxProfit = 0;
 
-        while(right<prices.length) {
-            if(prices[right]>prices[left]){
-                maxProfit=Math.max(maxProfit, prices[right]-prices[left]);
+        while (right < prices.length) {
+            if (prices[right] > prices[left]) {
+                maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
                 right++;
             } else {
-                left=right;
+                left = right;
                 right++;
             }
         }
